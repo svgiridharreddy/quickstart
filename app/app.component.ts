8600792ce093eb59a3104574f696f7eb8d4eda40
giridharr <giridar@codingmart.com>
2017-01-21 15:40:59 +0530

    new commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,4 +4,4 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent  { name = 'Giridhar'; }
+export class AppComponent  { name = 'Giridhar! How are you '; }
